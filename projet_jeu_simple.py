4b5844075d42d81af82e6e2ecc25dbb21a8e3ce8
Amina <amina.chaqchaq@gmail.com>
2022-04-20 15:28:52 +0200

    amina

--- projet_jeu_simple.py
+++ projet_jeu_simple.py
@@ -8,9 +8,14 @@ while  a <1 or a >10:
 
 print ("vous etes dans le bon intervale")
 if a%2==0:
-   if a < 5 :
+   if a < 5:
       print("Vous avez gagné un euro.")
    elif a > 5:
       print("Vous avez perdu 3 euros.")
-
-  
+else:
+    if a <= 5:
+       print("vous avez gagné 2 euro")
+    elif a == 5:
+        print("vous avez rien gagné")
+    else:
+        print("vous avez perdu 4 euro")
