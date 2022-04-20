1ebc8bd732ca4d3270bac9ef1c78a88c74693e7b
YacineWF3 <102653855+YacineWF3@users.noreply.github.com>
2022-04-20 14:18:17 +0200

    Add files via upload

--- projet_jeu_simple.py
+++ projet_jeu_simple.py
@@ -1,9 +1,13 @@
 a=0
-while  a <=1 or a >=10:
-       a = float(input("saisissez un nb compris entre 1 et 10:"))
-       if a <=1:
-          print("plus grand!!!")
-       elif a >=10:
-           print ("vous etes hors d'intervalle")
+while  a <1 or a >10:
+       a = float(input("saisissez un nombre compris entre 1 et 10:"))
+       if a <1:
+          print("Plus Grand")
+       elif a >10:
+           print ("Plus Petit ")
 
 print ("vous etes dans le bon intervale")
+if a%2==0:
+   print("Vous avez gagné 10 Euros")
+   if a<=3:
+      print("Vous avez perdu 5 euros")
