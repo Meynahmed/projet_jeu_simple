83c1c7508a561a1f32c23db9c000a5baddf85d92
gaye dramé <gayedrame92@gmail.com>
2022-04-20 13:54:59 +0200

    depot cours

---
+++ projet_jeu_simple.py
@@ -0,0 +1,9 @@
+a=0
+while  a <=1 or a >=10:
+       a = float(input("saisissez un nb compris entre 1 et 10:"))
+       if a <=1:
+          print("plus grand!!!")
+       elif a >=10:
+           print ("vous etes hors d'intervalle")
+
+print ("vous etes dans le bon intervale")
